f94e9ccc8235c5b4fc8ae3b476a62f08ce8928c1
szo94 <20407498+szo94@users.noreply.github.com>
2022-02-28 00:32:58 -0500

    Added basic analytics

--- endo.io/Program.cs
+++ endo.io/Program.cs
@@ -4,15 +4,32 @@ using System.Globalization;
 using System.IO;
 using CsvHelper;
 using System.Linq;
+using System.Threading;
 using CsvHelper.TypeConversion;
 
 namespace endo.io
 {
     internal class Program
     {
+        public const int DEF_TARGET_BG = 100;
+        public const int DEF_LOW_BG = 70;
+        public const int DEF_HIGH_BG = 180;
+
         static void Main(string[] args)
         {
+            BasalProfile profile1 = new BasalProfile("Profile 1",
+                new double[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            
             List<ClarityEvent> events = ReadCleanedClarityExport("C:\\Users\\shlom\\Downloads\\SampleClarityExport_Cleaned.csv");
+
+            int numOfReadings = events.Count;
+            double averageBG = events.Average(e => e.GlucoseValue);
+            double timeInRange = events.Count(e => e.GlucoseValue >= (profile1.LowBG ?? DEF_LOW_BG) &&
+                                                   e.GlucoseValue <= (profile1.HighBG ?? DEF_HIGH_BG)) / (double) numOfReadings;
+
+            Console.WriteLine($"Number of Readings: {numOfReadings}");
+            Console.WriteLine("Average BG: {0:F0}", averageBG);
+            Console.WriteLine("Time In Range: {0:P1}", timeInRange);
 
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
