0bef97f46445abaffc4e263c55ed97ef065cf18d
szo94 <20407498+szo94@users.noreply.github.com>
2022-03-01 21:59:29 -0500

    Added GetAverageByHour and PrintAverageByHour methods

--- endo.io/Program.cs
+++ endo.io/Program.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using CsvHelper;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using CsvHelper.TypeConversion;
 
@@ -11,9 +12,15 @@ namespace endo.io
 {
     internal class Program
     {
-        public const int DEF_TARGET_BG  = 100;
-        public const int DEF_LOW_BG     = 70;
-        public const int DEF_HIGH_BG    = 180;
+        const int DEF_TARGET_BG = 100;
+        const int DEF_LOW_BG = 70;
+        const int DEF_HIGH_BG = 180;
+        
+        static readonly string[] hour =
+        {
+            "12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM",
+            "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"
+        };
 
         static void Main(string[] args)
         {
@@ -21,12 +28,15 @@ namespace endo.io
             
             List<ClarityEvent> events = ReadCleanedClarityExport("C:\\Users\\shlom\\Downloads\\SampleClarityExport_Cleaned.csv");
 
-            double averageBG = events.Average(e => e.GlucoseValue);
-            double timeInRange = GetTimeInRange(events, testProfile);
+            double      averageBG       = events.Average(e => e.GlucoseValue);
+            double      timeInRange     = GetTimeInRange(events, testProfile);
+            double[]    averageByHour   = GetAverageByHour(events);
 
             Console.WriteLine($"Number of Readings: {events.Count}");
-            Console.WriteLine($"Average BG: {averageBG:F0}");
+            Console.WriteLine($"Average BG: {averageBG:F}");
             Console.WriteLine($"Time In Range: {timeInRange:P1}");
+            Console.WriteLine("Average By Hour:");
+            PrintAverageByHour(averageByHour);
 
             Console.WriteLine("\nPress any key to continue");
             Console.ReadKey();
@@ -56,6 +66,26 @@ namespace endo.io
             int readingsInRange = events.Count(e => e.GlucoseValue >= (basalProfile.LowBG ?? DEF_LOW_BG) &&
                                                     e.GlucoseValue <= (basalProfile.HighBG ?? DEF_HIGH_BG));
             return (double) readingsInRange / events.Count;
+        }
+
+        static double[] GetAverageByHour<T>(List<T> events) where T : Event
+        {
+            double[] AverageByHour = events
+                .GroupBy(e => e.Timestamp.Hour)
+                .Select(grp => grp.Average(e => e.GlucoseValue))
+                .ToArray();
+            return AverageByHour;
+        }
+
+        static void PrintAverageByHour(double[] hourlyAverages)
+        {
+            for (int i = 0; i < 24; i++)
+                Console.Write($"{hour[i],-6}");
+            Console.WriteLine();
+            for (int i = 0; i < (24 * 6); i++) { Console.Write("-"); }
+            Console.WriteLine();
+            for (int i = 0; i < 24; i++)
+                Console.Write($"{hourlyAverages[i],-6:F1}");
         }
     }
 }
