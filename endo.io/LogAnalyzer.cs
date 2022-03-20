49981b7325aa999401f915a55be13f18427620d2
szo94 <20407498+szo94@users.noreply.github.com>
2022-03-20 17:00:04 -0400

    Implemented offset for basal suggestions

--- endo.io/LogAnalyzer.cs
+++ endo.io/LogAnalyzer.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using CsvHelper;
-using CsvHelper.TypeConversion;
-using NUnit.Framework;
 
 namespace endo.io
 {
@@ -14,7 +13,7 @@ namespace endo.io
         private const int DEF_TARGET_BG = 100;
         private const int DEF_LOW_BG = 70;
         private const int DEF_HIGH_BG = 180;
-        private const int DEF_OFFSET = 1;
+        private const int OFFSET = 1;
 
         public static readonly string[] HOURS =
         {
@@ -22,21 +21,26 @@ namespace endo.io
             "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"
         };
 
-
         public PatientProfile Profile { get; }
-        public List<ClarityEvent> EventLog { get; private set; }
+        public List<ClarityEvent> EventLog { get; }
         public double AverageBG { get; private set; }
         public double TimeInRange { get; private set; }
         public double[] AverageByHour { get; private set; }
         public double[] VarianceByHour { get; private set; }
         public double[] BasalSuggestions { get; private set; }
 
-        public LogAnalyzer(PatientProfile profile, string filepath)
+        public LogAnalyzer(PatientProfile profile, string filePath)
         {
             Profile = profile;
+
             try
             {
-                ReadCleanedClarityExport(filepath);
+                using (var reader = new StreamReader(filePath))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Context.RegisterClassMap<ClarityEventMap>();
+                    EventLog = csv.GetRecords<ClarityEvent>().ToList();
+                }
                 Analyze();
             }
             catch (IOException)
@@ -46,16 +50,6 @@ namespace endo.io
             catch (Exception ex)
             {
                 Console.WriteLine($"Failed to copy events: {ex.GetType()}");
-            }
-        }
-
-        private void ReadCleanedClarityExport(string filePath)
-        {
-            using (var reader = new StreamReader(filePath))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                csv.Context.RegisterClassMap<ClarityEventMap>();
-                EventLog = csv.GetRecords<ClarityEvent>().ToList();
             }
         }
 
@@ -87,8 +81,12 @@ namespace endo.io
         private double[] GetBasalSuggestions(double[] varianceByHour)
         {
             double[] basalSuggestions = new double[24];
-            for (int i = 0; i < 24; i++)
-                basalSuggestions[i] = (varianceByHour[i] / (Profile.TargetBG ?? DEF_TARGET_BG)) * (Profile.BasalRates[i]);
+            for (int i = 0; i < 23; i++)
+            {
+                int targetHour = (i - OFFSET + 24) % 24; // offset -1 hrs
+                basalSuggestions[targetHour] = (varianceByHour[i] / (Profile.TargetBG ?? DEF_TARGET_BG)) * Profile.BasalRates[i];
+            }
+
             return basalSuggestions;
         }
     }
