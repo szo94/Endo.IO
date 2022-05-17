254de48d47e61a1cb92e34176e9476a220cf4287
szo94 <20407498+szo94@users.noreply.github.com>
2022-05-17 00:15:25 -0400

    Fixed password verification bug

--- endo.io/Program.cs
+++ endo.io/Program.cs
@@ -1,60 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 using endo.io.Data;
-using endo.io.Testing;
 using static endo.io.Constants.Constants;
 
 namespace endo.io
 {
     internal class Program
     {
-        // get reference to database object (singleton)
+        // reference to database
         static LinqToSqlDatabase db = LinqToSqlDatabase.Instance;
 
         [STAThread]
         private static void Main()
         {
-            // get username
+            // prompt user for username
             Console.WriteLine("Welcome to Endo.IO!\n");
             Console.Write("Enter username: ");
             string userName = Console.ReadLine();
             while (!db.UserExists(userName))
             {
-                Console.WriteLine("User not found.\nPlease enter a valid username: ");
+                Console.Write("User not found.\nPlease enter a valid username: ");
                 userName = Console.ReadLine();
             }
 
-            // get password
+            // prompt user for password
             Console.Write("Enter password: ");
             string password = Console.ReadLine();
             while (!db.VerifyLoginCredentials(userName, password))
             {
-                Console.WriteLine("Password incorrect.\nPlease try again: ");
-                userName = Console.ReadLine();
+                Console.Write("Password incorrect.\nPlease try again: ");
+                password = Console.ReadLine();
             }
+            Console.Clear();
 
             // fetch user profile from database
             UserProfile profile = db.GetUserProfile(userName);
 
-            // open file explorer for user to select input file
-            string filePath = GetInputFilePath();
+            // open file explorer and let user select input file
+            string filePath = SelectInputFile();
 
             // read clarity export
             ClarityExportReader reader = new ClarityExportReader(filePath);
-            List<ClarityEvent> eventLog = null;
             try
             {
-                eventLog = reader.ReadFile();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex is IOException ? "Failed to read file" : $"Failed to copy events: {ex.GetType()}");
-            }
+                List<ClarityEvent> eventLog = reader.ReadFile();
 
-            if (eventLog != null)
-            {
                 // analyze event data
                 LogAnalyzer analyzer = new LogAnalyzer(profile, eventLog);
 
@@ -70,16 +63,23 @@ namespace endo.io
                 Console.WriteLine($"Average BG:{analyzer.AverageBG,16:F}");
                 Console.WriteLine($"Time In Range:{analyzer.TimeInRange,13:P1}");
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine((ex is IOException ? "Failed to read file " : "Failed to copy events ")
+                                  + ex.GetType());
+            }
 
             Console.WriteLine("\nPress any key to continue");
             Console.ReadKey();        
         }
 
-        private static string GetInputFilePath()
+        private static string SelectInputFile()
         {
             string filePath = "";
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Title = "Open Clarity Export";
+            ofd.InitialDirectory =
+                Path.Combine(Assembly.GetExecutingAssembly().Location, @"..\..\..\TestFiles");
             ofd.Filter = "CSV Files |*.csv";
             if (ofd.ShowDialog() == DialogResult.OK)
             {
