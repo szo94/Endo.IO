768565babf5de62c7dad3376940b299aa2a83f74
szo94 <20407498+szo94@users.noreply.github.com>
2022-05-22 14:40:31 -0400

    Added ClarityExportReader unit tests

---
+++ endo.io/UnitTests/ClarityExportReaderTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Endo.IO;
+using System;
+using System.IO;
+
+namespace endo.io.Testing
+{
+    [TestFixture]
+    public class ClarityExportReaderTests
+    {
+        [Test]
+        public void ReadFileOnProperlyFormattedFileReturnsListOfEvents()
+        {
+            ClarityExportReader reader = new ClarityExportReader(Path.Combine(
+                Environment.CurrentDirectory,
+                @"endo.io\UnitTests\",
+                "SampleClarityExport_Cleaned.csv"));
+            Assert.IsNotEmpty(reader.ReadFile());
+        }
+
+        [Test]
+        public void ReadFileOnImproperlyFormattedFileThrowsCsvHelperReaderException()
+        {
+            ClarityExportReader reader = new ClarityExportReader(Path.Combine(
+                Environment.CurrentDirectory,
+                @"endo.io\UnitTests\",
+                "SampleClarityExport_Uncleaned.csv"));
+            Assert.Throws<CsvHelper.ReaderException>(() => reader.ReadFile());
+        }
+    }
+}
